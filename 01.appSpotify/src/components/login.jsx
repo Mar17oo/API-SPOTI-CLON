ce8c40fb783ee2b0c747f35cbec31e603ad15679
Mar Bautista <mar_piscis@live.com.mx>
2023-09-05 15:53:31 -0600

    se conecta el login con Spotify

--- 01.appSpotify/src/components/login.jsx
+++ 01.appSpotify/src/components/login.jsx
@@ -1,41 +1,54 @@
 import React from 'react';
 import styled from "styled-components";
+
 export default function Login() {
-    const handleClick = () => {
-        const cliendId = "2d18743702b647cc9bdb376d8d159235";
-        const redirectUrl = "http://localhost:3000/";
-        const apiurl = "http://accounts.spotify.com/authorize";
-        const scope = ['user-read-email',]
-    }
-    return (
+    const handleClick = async () => {
+    const client_id = "b10ac7cc459d474e961a6603c15da715";
+    const redirect_uri = "https://spotify-clone-api.netlify.app/";
+    const api_uri = "https://accounts.spotify.com/authorize";
+    const scope = [
+        "user-read-private",
+        "user-read-email",
+        "user-modify-playback-state",
+        "user-read-playback-state",
+        "user-read-currently-playing",
+        "user-read-recently-played",
+        "user-top-read",
+    ];
+    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
+        " "
+    )}&response_type=token&show_dialog=true`;
+    };
+return (
     <Container>
         <img
-         src ="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
-         alt="Logo spotify"
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
+        alt="spotify"
         />
         <button onClick={handleClick}>Connect Spotify</button>
     </Container>
-    )
+    );
 }
 
-const Container = styled.di`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-height: 100vh;
-width: 100vw;
-background-color: green;
-gap: 5 rem;
-img{
-    height: 20vh
-}
-button{
-    padding: 1rem 5rem;
-    border-radius: 5rem;
-    border: none;
-    background-colot: black;
-    color: #49f585;
-    font-size: 1.4rem;
-    cursor: ponter;
-}
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    background-color: #1db954;
+    gap: 5rem;
+    img {
+        height: 20vh;
+    }
+    button {
+        padding: 1rem 5rem;
+        border-radius: 5rem;
+        background-color: black;
+        color: #49f585;
+        border: none;
+        font-size: 1.4rem;
+        cursor: pointer;
+    }
+`;
