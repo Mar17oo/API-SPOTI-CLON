09fa464f52964bda3cae29b3f129fdd4a795b6f6
Mar Bautista <mar_piscis@live.com.mx>
2023-09-05 15:36:46 -0600

    Crando login y añadiendo scope

--- 01.appSpotify/src/components/login.jsx
+++ 01.appSpotify/src/components/login.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import styled from "styled-components";
-export default function Login(){
-    return <Container>
+export default function Login() {
+    const handleClick = () => {
+        const cliendId = "2d18743702b647cc9bdb376d8d159235";
+        const redirectUrl = "http://localhost:3000/";
+        const apiurl = "http://accounts.spotify.com/authorize";
+        const scope = ['user-read-email',]
+    }
+    return (
+    <Container>
         <img
          src ="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
          alt="Logo spotify"
         />
+        <button onClick={handleClick}>Connect Spotify</button>
     </Container>
-
+    )
 }
 
 const Container = styled.di`
