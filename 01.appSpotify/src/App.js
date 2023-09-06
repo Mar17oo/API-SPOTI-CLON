3aa79f339bc6ec597abf75ca7d405553e235b857
Mar Bautista <mar_piscis@live.com.mx>
2023-09-05 19:14:11 -0600

    Se termina de usar la api para login

--- 01.appSpotify/src/App.js
+++ 01.appSpotify/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import Login from "./components/login.jsx"
+import {reducerCases} from "./utils/Constants.js"
+import {useStateProvider} from "./utils/StateProvider.jsx"
 
 export default function App() {
-  return(
-    <div>App</div>
+  useEffect(() =>{
+  const hash = window.lococation.hash;
+  if (hash) {
+    const token = hash.substring(1).split("&")[0].split("=")[1];  
+    dispatchEvent({ type: reducerCases.SET_TOKEN, token })
+  }
+}, [token, dispatch]);
+  return (
+    <div> 
+      <Login/>
+    </div>
   )
 }
 
---
+++ 01.appSpotify/src/utils/Constants.js
@@ -0,0 +1,6 @@
+export const reducerCases = {
+    token:null,
+};
+
+
+const reducer = ()
